f4cb01e59a4c65c8bddb9f3f9652c3def35ce50c
Saad Ahmed <saadahmedscse@gmail.com>
2022-02-27 18:10:48 +0600

    Initial Commit

--- app/src/main/java/com/caffeine/eirmon/view/activity/AuthenticationActivity.kt
+++ app/src/main/java/com/caffeine/eirmon/view/activity/AuthenticationActivity.kt
@@ -1,11 +1,12 @@
-package com.caffeine.eirmon
+package com.caffeine.eirmon.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.caffeine.eirmon.R
 
-class MainActivity : AppCompatActivity() {
+class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_authentication)
     }
 }
---
+++ app/src/main/java/com/caffeine/eirmon/view/activity/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.caffeine.eirmon.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.caffeine.eirmon.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
