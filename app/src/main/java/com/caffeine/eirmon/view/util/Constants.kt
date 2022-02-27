82aa90fb77bf8a6ebeca0867e033ce1f8492b37b
Saad Ahmed <saadahmedscse@gmail.com>
2022-02-27 18:31:33 +0600

    Added Toast

--- app/src/main/java/com/caffeine/eirmon/view/util/Constants.kt
+++ app/src/main/java/com/caffeine/eirmon/view/util/Constants.kt
@@ -1,7 +1,9 @@
 package com.caffeine.eirmon.view.util
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
@@ -11,9 +13,15 @@ object Constants {
     val auth = FirebaseAuth.getInstance()
     val currentUser = auth.currentUser
     val UID = auth.uid
+    const val LONG = 1
+    const val SHORT = 0
 
     fun intentToActivity(activity : Activity, c : Class<*>){
         activity.startActivity(Intent(activity, c))
         activity.finish()
+    }
+
+    fun showToast(context : Context, message : String, duration : Int){
+        Toast.makeText(context, message, duration).show()
     }
 }
