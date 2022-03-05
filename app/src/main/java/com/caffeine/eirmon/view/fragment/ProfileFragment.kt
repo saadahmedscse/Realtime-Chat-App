c086a9b93d088cbed382863357b63b29b6cfabd0
Saad Ahmed <saadahmedscse@gmail.com>
2022-03-05 20:42:06 +0600

    profile functionalities

--- app/src/main/java/com/caffeine/eirmon/view/fragment/ProfileFragment.kt
+++ app/src/main/java/com/caffeine/eirmon/view/fragment/ProfileFragment.kt
@@ -5,56 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import com.caffeine.eirmon.R
+import com.caffeine.eirmon.databinding.FragmentProfileBinding
+import com.caffeine.eirmon.service.model.UserModel
+import com.caffeine.eirmon.util.AlertDialog
+import com.caffeine.eirmon.util.Constants
+import com.caffeine.eirmon.util.DataState
+import com.caffeine.eirmon.view.activity.AuthenticationActivity
+import com.caffeine.eirmon.viewmodel.UserViewModel
+import com.squareup.picasso.Picasso
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentProfileBinding
+    private val viewModel : UserViewModel by viewModels()
+    var user = UserModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        viewModel.getMyDetails()
+
+        viewModel.myLiveData.observe(viewLifecycleOwner) {
+            when (it){
+                is DataState.Loading -> {}
+
+                is DataState.Success -> {
+                    user = it.data!!
+                    if (user.picture != "null"){
+                        Picasso.get().load(user.picture).into(binding.profilePicture)
+                    }
+
+                    binding.name.text = user.name
+                    binding.email.text = user.email
                 }
+
+                is DataState.Failed -> {
+                    Constants.showSnackBar(requireContext(), binding.root, it.message!!, Constants.SNACK_LONG)
+                }
+
             }
+        }
+
+        binding.logout.setOnClickListener{
+            Constants.auth.signOut()
+            Constants.intentToActivity(requireActivity(), AuthenticationActivity::class.java)
+        }
+
+        binding.editProfile.setOnClickListener{
+            val action = ProfileFragmentDirections.profileToEditProfile(user)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        return binding.root
     }
 }
